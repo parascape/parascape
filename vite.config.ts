5822a32877e5290d682234619414350060badace
parascape <recordsparascape@gmail.com>
2025-02-12 01:41:10 +0000

    chore: revert to github pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => ({
-  base: '/',
+  base: '/parascape/',
   plugins: [react()],
   resolve: {
     alias: {
