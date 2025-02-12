7b9e6ae38b9e721d1cdbf1f20bdaf31f73445d77
parascape <recordsparascape@gmail.com>
2025-02-12 01:23:50 +0000

    Updates

--- assets/js/ui-label-CZqMSgr_.js
+++
@@ -1,2 +0,0 @@
-import{r,j as t,m as a}from"./vendor-react-Dc1AoWZo.js";import{b as m}from"./vendor-common-lQ54BdYb.js";import{c as i}from"./ui-button-DAQ7LkdU.js";const l=m("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),p=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:i(l(),s),...e}));p.displayName=a.displayName;export{p as L};
-//# sourceMappingURL=ui-label-CZqMSgr_.js.map
--- assets/js/ui-suspense-boundary-MoDh_VO2.js
+++
@@ -1,2 +0,0 @@
-import{j as s,r as o}from"./vendor-react-Dc1AoWZo.js";import{L as p}from"./ui-loading-DxIhPxiq.js";function x({children:a,fallback:r,loadingText:t,fullscreen:e=!1}){const n=s.jsx(p,{variant:"spinner",text:t,fullscreen:e});return s.jsx(o.Suspense,{fallback:r||n,children:a})}export{x as S};
-//# sourceMappingURL=ui-suspense-boundary-MoDh_VO2.js.map
